c59afabe311bcec2f13789fda129884dde38907f
William Light <git@wrl.lhiaudio.com>
2020-12-01 05:40:09 +0100

    fix SilenceProcessingTest when number of outputs > number of inputs
    this loop sets a silence flag for all of the channels in
    processData.outputs[busIndex]. however, it takes the channel count from
    processData.inputs[busIndex].numChannels.
    
    when there are more output buses than input buses in a plugin, this
    resulted in an invalid memory read past the end of processData.inputs.

--- source/vst/testsuite/processing/silenceprocessing.cpp
+++ source/vst/testsuite/processing/silenceprocessing.cpp
@@ -125,7 +125,7 @@ bool PLUGIN_API SilenceProcessingTest::run (ITestResult* testResult)
 				{
 					processData.outputs[busIndex].silenceFlags = 0;
 					for (int32 channelIndex = 0;
-					     channelIndex < processData.inputs[busIndex].numChannels; channelIndex++)
+					     channelIndex < processData.outputs[busIndex].numChannels; channelIndex++)
 						processData.outputs[busIndex].silenceFlags |= (uint64)1
 						                                              << (uint64)channelIndex;
 				}
